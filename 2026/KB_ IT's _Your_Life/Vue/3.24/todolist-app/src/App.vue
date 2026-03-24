579a24d6c56b9742002abb0889bbb3da57330ad2
Park Geon Woo <parkgw8290@gmail.com>
2026-03-24 16:20:07 +0900

    열심히 따라가는중

---
+++ 2026/KB_ IT's _Your_Life/Vue/3.24/todolist-app/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import CheckboxItem from './components/CheckboxItem.vue';
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+  data() {
+    return {
+      idols: [
+        { id: 1, name: 'BTS', checked: true },
+        { id: 2, name: 'Black Pink', checked: false },
+        { id: 3, name: 'EXO', checked: false },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <CheckboxItem v-for="idol in idols" 
+  : key="idol.id"
+  : id="idol.id"
+  :name="idol.name"
+  :checked="idol.checked"
+  @check-changed="toggleIdol">
+
+</CheckboxItem>
+
+ 
+</template>
+
+
