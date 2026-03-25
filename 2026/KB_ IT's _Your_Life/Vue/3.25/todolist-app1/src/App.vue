d1652c714c825a5d9a9e7c6e0bb2eefa0e7b6427
Park Geon Woo <parkgw8290@gmail.com>
2026-03-25 16:21:42 +0900

    잘 따라가고있어요

--- 2026/KB_ IT's _Your_Life/Vue/3.25/todolist-app1/src/App.vue
+++ 2026/KB_ IT's _Your_Life/Vue/3.25/todolist-app1/src/App.vue
@@ -1,47 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <h1>건우의 todo앱</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <ul>
+      <TodoListItem v-for="item in todoList" :key="item.id" :todoItem="item" />
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import TodoListItem from './components/TodoListItem.vue';
+export default {
+  name: 'App',
+  components: {
+    TodoListItem,
+  },
+  data() {
+    return {
+      todoList: [
+        { id: 1, todo: 'Vue 실습하기', completed: false },
+        { id: 2, todo: '자식 컴포넌트 이해하기', completed: true },
+        { id: 3, todo: '맛있는 저녁 먹기', completed: false },
+      ],
+    };
+  },
+};
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
