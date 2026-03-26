8d95fac107f7c3b5e20cba69f78aac63cfdff9e5
Park Geon Woo <parkgw8290@gmail.com>
2026-03-26 16:20:35 +0900

    7교시

--- 2026/KB_ IT's _Your_Life/Vue/3.26/slot-test/src/main.js
+++ 2026/KB_ IT's _Your_Life/Vue/3.26/slot-test/src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
-import App from './App3.vue'
+import App from './App5.vue'
 
 createApp(App).mount('#app')
