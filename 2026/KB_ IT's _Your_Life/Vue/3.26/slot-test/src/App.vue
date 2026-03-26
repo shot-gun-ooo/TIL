116de38eeda2c9d4aebf7b0cce00ce3c0f9ffd93
Park Geon Woo <parkgw8290@gmail.com>
2026-03-26 14:15:19 +0900

    주강사님 시간

--- 2026/KB_ IT's _Your_Life/Vue/3.26/slot-test/src/App.vue
+++ 2026/KB_ IT's _Your_Life/Vue/3.26/slot-test/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-
-
-    
+    <NoSlotTest />
+    <br />
+    <br />
+    <NoSlotTest2 />
+    <br />
+    <br />
   </div>
 </template>
 
 <script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import NoSlotTest2 from './components/NoSlotTest2.vue';
 export default {
   name: 'App',
-  components: {},
+  components: { NoSlotTest: NoSlotTest, NoSlotTest2 },
 };
 </script>
 
