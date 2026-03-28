adff336959fd1e18de05f225e29d78a930270fca
Park Geon Woo <parkgw8290@gmail.com>
2026-03-28 22:32:12 +0900

    전반적 흐름 공부

---
+++ 2026/KB_ IT's _Your_Life/Vue/3.28/practice/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <child :msg="hello" />
+    <button @click="changeMessage">메시지 변경</button>
+    <h1>우리 집 거실</h1>
+    <LightSwitch :isOn="isLightOn" @toggle-light="handleUpdate" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import child from './components/child.vue';
+import LightSwitch from './components/LightSwitch.vue';
+
+const hello = ref('안녕하세요');
+const changeMessage = () => {
+  hello.value = '토요일이 끝나가요';
+};
+const isLightOn = ref(false);
+const handleUpdate = () => {
+  isLightOn.value = !isLightOn.value;
+};
+</script>
+
+<style></style>
---
+++ 2026/KB_ IT's _Your_Life/Vue/3.28/practice/src/components/child.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>두구두구: {{ msg }}</h3>
+  </div>
+</template>
+
+<script setup>
+defineProps(['msg']);
+</script>
+
+<style></style>
