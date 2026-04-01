c4d395fbc11da8dfbccbb1fc35a9edc2dd0e0b94
Park Geon Woo <parkgw8290@gmail.com>
2026-04-01 17:19:28 +0900

    1차과제제출

---
+++ 2026/KB_ IT's _Your_Life/Vue/30회차_1주차과제_박건우/router-test/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <Header />
+
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
--- 2026/KB_ IT's _Your_Life/Vue/4.1/router-test2/src/App.vue
+++ 2026/KB_ IT's _Your_Life/Vue/4.1/router-test2/src/App.vue
@@ -1,85 +1,40 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <Header />
+    <router-view> </router-view>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import Header from '@/components/Header.vue';
+import { provide } from 'vue';
+import Videos from '@/videos.json';
+provide();
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+.container {
   text-align: center;
-  margin-top: 2rem;
+  margin-top: 10px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.fa {
+  padding: 5px;
+  width: 30px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 2px;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.fa-facebook {
+  background: #3b5998;
+  color: white;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.fa-youtube {
+  background: #bb0000;
+  color: white;
 }
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.fa-instagram {
+  background: #125688;
+  color: white;
 }
 </style>
