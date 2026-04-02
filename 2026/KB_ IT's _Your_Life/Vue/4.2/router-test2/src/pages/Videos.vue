32f558dca6982eef609067c90a0028e4686cf4ee
Park Geon Woo <parkgw8290@gmail.com>
2026-04-02 10:37:52 +0900

    4.2수업

---
+++ 2026/KB_ IT's _Your_Life/Vue/4.2/router-test2/src/pages/Videos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card card-body">
+    <h2>Videos</h2>
+    <ul class="list-group">
+      <li v-for="v in videos" :key="v.id" class="list-group-item text-left">
+        {{ v.title }} <span class="float-end badge bg-secondary">듣기</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+export default {
+  name: 'Videos',
+  setup() {
+    const videos = inject('videos');
+
+    return { videos };
+  },
+};
+</script>
+
+<style></style>
