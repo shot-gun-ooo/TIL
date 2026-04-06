ff03f9f03a9de8033c78d767a51a3f7ac4daaec0
Park Geon Woo <parkgw8290@gmail.com>
2026-04-06 13:38:24 +0900

    오후

---
+++ 2026/KB_ IT's _Your_Life/Vue/4.6/todolist-app-router_axios_pinia적용전/todolist-app-router_axios_pinia적용전/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <Header />
+    <router-view />
+    <Loading v-if="todoStore.states.isLoading" />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Loading from './components/Loading.vue';
+import { useTodoListStore } from './stores/todoList'; // 스토어 설계도 불러오기
+
+// 1. 설계도(useTodoListStore)를 실행해서 실제 데이터 바구니(todoStore)를 만듭니다.
+const todoStore = useTodoListStore(); 
+
+// 2. 이제 template에서는 위에서 만든 'todoStore'라는 이름만 사용하면 됩니다.
+</script>
---
+++ 2026/KB_ IT's _Your_Life/Vue/4.6/todolist-app-router_axios_pinia적용전/todolist-app-router_axios_pinia적용전/src/pages/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div class="col p-3">
+      <router-link class="btn btn-primary" to="/todos/add">
+        할일 추가
+      </router-link>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoItem
+          v-for="todoItem in todoListStore.states.todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TodoItem from '@/pages/TodoItem.vue';
+import { useTodoListStore } from '@/stores/todoList';
+
+// Pinia 스토어 불러오기
+const todoListStore = useTodoListStore();
+
+// 만약 화면이 처음에 비어있다면, 여기서 데이터를 한 번 불러와야 할 수도 있습니다.
+// todoListStore.fetchTodoList(); 
+</script>
