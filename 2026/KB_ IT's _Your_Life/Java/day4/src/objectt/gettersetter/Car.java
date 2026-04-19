670adbf8d60186c31af7d0c5ec40f7804a3cce38
Park Geon Woo <parkgw8290@gmail.com>
2026-04-19 15:53:30 +0900

    4.19

--- 2026/KB_ IT's _Your_Life/Java/day4/src/objectt/gettersetter/Car.java
+++ 2026/KB_ IT's _Your_Life/Java/day4/src/objectt/gettersetter/Car.java
@@ -6,9 +6,18 @@ public class Car {
     private boolean start;
     private int speed;
     private String color = "검정";
+    private int maxSpeed = 350;
 
     public String getModel(){
         return model;
+    }
+
+    public void setSpeed(int speed){
+        if(speed < 0 || speed>maxSpeed){
+            System.out.println(speed);
+            return;
+        }
+       this.speed = speed;
     }
 
     public void setColor(String color){
