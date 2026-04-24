2d20301a040e0ae20d58a7f036902d45dd46eb05
Park Geon Woo <parkgw8290@gmail.com>
2026-04-24 10:40:43 +0900

    4.24.10am

--- 2026/KB_ IT's _Your_Life/Java/demo1/src/main/java/com/example/demo/Demo1Application.java
+++ 2026/KB_ IT's _Your_Life/Java/demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -8,6 +8,23 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+
+
     }
+
+    public void sayHello() {
+        System.out.println("Hello from Demo1Application!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public String getStatus() {
+        return "Application is running";
+    }
+
+
+
 
 }
