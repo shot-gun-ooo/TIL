e87087377f32031a0defed53a1c7d48479bef699
Park Geon Woo <parkgw8290@gmail.com>
2026-04-30 11:32:26 +0900

    4.30.11am

---
+++ 2026/KB_ IT's _Your_Life/Java/day11/src/test/Card.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Card {
+    public static void main(String[] args) {
+        //카드 문제는
+        //rule1) 카드수를 입력 : 6
+        //rule2) 맨앞에 있는 카드를 제거한다.
+        //rule3) 두번째 있는 카드를 맨끝으로 붙인다.
+        // --> rule2-3을 반복한 후 마지막에 남은 카드 번호가 몇 번입니까
+    }
+}
